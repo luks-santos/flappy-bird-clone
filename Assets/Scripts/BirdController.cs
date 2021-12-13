2c6e005a660d8aad1b38c04bc27c17e05a5e9a2b
Lucas Batista <lucas.bt46@gmail.com>
2021-12-13 18:39:37 -0300

    fisica bird, collider ground e pipes

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -2,20 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BirdControl : MonoBehaviour
+public class BirdController : MonoBehaviour
 {
     private Animator flap;
+    public float speed = 1f;
+    private Rigidbody2D rgb2d;
 
     // Start is called before the first frame update
     void Start()
     {
         flap = GetComponent<Animator>();
         flap.SetBool("isFly", true);
+        rgb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space)) {
+            rgb2d.velocity = Vector2.up * speed;
+        }
     }
 }
